585842720affa6a98e199f7fa0474bc552686270
Paul Peelen <paul@paulpeelen.com>
2022-04-05 21:49:20 +0200

    Initial Commit

---
+++ ParkingPlay/ParkingPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingPlayApp.swift
+//  ParkingPlay
+//
+//  Created by Paul Peelen on 2022-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ParkingPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
